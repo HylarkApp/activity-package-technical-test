3917925eef5c21ca74b841b80a246825d5605ec5
Angel <angeldimov777@gmail.com>
2025-06-19 13:54:55 +0100

    Added activity logging code

--- database/migrations/2022_01_01_000001_create_actions_table.php
+++ database/migrations/2022_01_01_000001_create_actions_table.php
@@ -15,7 +15,33 @@ return new class extends Migration {
         Schema::create('actions', function(Blueprint $table) {
             $table->id();
 
-            // TODO: Create actions table
+            // The user who performed the action
+            // We can use nullable()->index() to allow for actions that are not performed by a user (cron, system, etc.)
+            $table->foreignId('performer_id')->constrained('users')->nullOnDelete();
+            $table->string('performer_type');
+            // * $table->string('performer_name')->nullable();
+            $table->string('subject_type');
+            $table->unsignedBigInteger('subject_id');
+
+            // Here is the action type (in pur case: created, updated and deleted). We can use enum for more strict validation or json field for more flexibility.
+            $table->string('action_type');
+
+            //TODO: Improvements (*):
+            // JSON field for before/after data (can be null at first)
+            // $table->json('versions')->nullable();
+            
+            // Example of payload: 
+            // {
+            //     "old": {
+            //         "title": "Old Title",
+            //         "body": "Some old content"
+            //     },
+            //     "new": {
+            //         "title": "New Title",
+            //         "body": "Some updated content"
+            //     }
+            // }
+
 
             $table->timestamps();
         });
